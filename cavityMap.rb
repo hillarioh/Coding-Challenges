6cf86143383d70a9587b7022e00bf2772ecd018b
hillarioh <okerioh@gmail.com>
2020-06-08 18:12:36 +0300

    remove errors

--- cavityMap.rb
+++ cavityMap.rb
@@ -7,11 +7,14 @@ def cavityMap(grid)
     grid[i].each_char.with_index do  |v,k|
       
         if k>0 && k< size-1
-          
-          next if(v.to_i - grid[i-1][k].to_i) < 0
-          next if(v.to_i - grid[i+1][k].to_i)<0
-          next if(v.to_i-grid[i][k-1].to_i)<0
-          next if(v.to_i-grid[i][k+1].to_i )<0 
+          next if grid[i-1][k]=='X'
+          next if(v.to_i - grid[i-1][k].to_i) < 1 
+          next if grid[i+1][k]=='X'
+          next if(v.to_i - grid[i+1][k].to_i)<1 
+          next if grid[i][k-1]=='X'
+          next if(v.to_i-grid[i][k-1].to_i)<1 
+          next if  grid[i][k+1]=='X'
+          next if(v.to_i-grid[i][k+1].to_i )<1 
           grid[i][k]='X'          
         end
        
@@ -26,6 +29,15 @@ end
 grid2 = ["989","191","111"]
 grid1 = ["1112","1912","1892","1234"]
 
+grid4=["179443854",
+  "961621369",
+  "164139922",
+  "968633951",
+  "812882578",
+  "257829163",
+  "812438597",
+  "176656233",
+  "485773814"]
 
  
-p cavityMap(grid1)
+p cavityMap(grid4)
