b9b22466d4dcfc5d249feb048a4eb7c54f7dd6bf
hillarioh <okerioh@gmail.com>
2020-07-16 07:25:18 +0300

    edit singlelinked.rb

--- singlylinked.rb
+++ singlylinked.rb
@@ -1,48 +1,95 @@
 # frozen_string_literal: true
 
 class Node
-  attr_accessor :data, :next_node
-  def initialize(data, next_node = nil)
-    @data = data
+  attr_accessor :value, :next_node
+  def initialize(value, next_node = nil)
+    @value = value
     @next_node = next_node
   end
 
-#   def self.count_nodes(head)
-
-#     result = 1
-#     current_node = head
-
-#     while current_node.next_node != nil
-#       result += 1
-#       current_node = current_node.next_node
-#     end
-
-#     result
-# end
-
-
 end
 
-def count_nodes(head)
-
-    result = 1
-    current_node = head
-
-    while current_node.next_node != nil
-      result += 1
-      current_node = current_node.next_node
+class LinkedList
+    def initialize
+        @head = nil
+        @tail = nil        
     end
 
-    result
+    def add(d) 
+        if @head==nil
+            @head = Node.new(d)
+            @tail = Node.new(d)
+            return     
+        end
+
+        n = @head
+        while n.next_node != nil
+            n = n.next_node;
+        end
+        n. next_node= Node.new(d)
+    end
+
+    def count_nodes
+
+        result = 1
+        current_node = @head
+    
+        while current_node.next_node != nil
+          result += 1
+          current_node = current_node.next_node
+        end
+    
+        result
+    end
+
+    def get(index)
+        # your code here
+        i=0 
+       current_node = @head
+        
+            while current_node.next_node != nil
+              if index==i
+                return current_node.value
+              end
+              i+=1
+              current_node = current_node.next_node
+            end
+
+           current_node.value if !@head.nil?
+
+        #    other method
+
+        #    current_node=@head
+           # while i<index
+           #    current_node= current_node.next_node
+           #   i+=1
+           # end
+           #  current_node.value
+      end
+
 end
 
-head = Node.new(4)
-node_b = Node.new(2)
-node_c = Node.new(3)
-node_d = Node.new(10)
 
-head.next_node = node_b
-node_b.next_node = node_c
-node_c.next_node = node_d
 
-puts count_nodes(head)
+# head = Node.new(4)
+# node_b = Node.new(2)
+# node_c = Node.new(3)
+# node_d = Node.new(10)
+
+# head.next_node = node_b
+# node_b.next_node = node_c
+# node_c.next_node = node_d
+
+
+
+myList = LinkedList.new
+myList.add(2)
+myList.add(6)
+myList.add(9)
+myList.add(1)
+puts myList.count_nodes
+puts myList.get(3)
+
+
+
+
